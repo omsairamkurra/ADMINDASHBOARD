1937c797eeee4fee29196d3d4e9944039a4939d9
Ak8582 <Kurraomsairam2@gmail.com>
2023-10-22 20:35:54 +0530

    commit

---
+++ Dashboard/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+import { Home } from './components/Home'
+import {useState} from 'react'
+
+function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+  return (
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
